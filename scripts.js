80d4d94f59a62f8efda6c6d88b6cc922f8e11109
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-02-02 13:50:30 -0300

    Inserção do número de cartas escolhidas

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 
-let cartas = prompt("Com quantas cartas quer jogar?"); 
-while (cartas > 14  || cartas < 2) {
+ /*Comandos de entrada de cartas */
+
+let escolhecartas = prompt("Com quantas cartas quer jogar?"); 
+/*while (cartas > 14  || cartas < 2) {
     cartas = prompt("Com quantas cartas quer jogar?"); 
+
+} */ //Habilitar depois
+// alert (escolhecartas);
+
+let cartasjogo = 14 - escolhecartas;
+let aux = 1;
+// alert (cartasjogo);
+ while (cartasjogo > 0) {
+    let removerCarta = document.querySelector(".carta" + aux);
+    removerCarta.classList.add("remove")
+    aux = aux + 1;
+    cartasjogo = cartasjogo - 1;
+} 
+
+function virarCarta(parametro) {
+    let flip = document.querySelector("."+parametro )
+    flip.classList.add("verde");
 }
-alert (cartas);
