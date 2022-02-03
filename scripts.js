3470080bbcea8d54af1fc8d91752d2c47ad13e53
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-02-03 13:02:02 -0300

    Projeto com problemas no layout após habilitar o flip da carta. Rodrigo ficou de ver o projeto nessa parte

--- scripts.js
+++ scripts.js
@@ -1,24 +1,44 @@
+let baralho = ['<img src="Gifs/bobrossparrot.gif">'];
 
+function carregarBaralho () {
+  const carta = document.querySelector("main");
+  for (let i = 0; i < baralho.length; i++) {
+    carta.innerHTML = carta.innerHTML + `
+    <div class="flip carta${i} card" onclick="fliparCarta('carta${i}')">
+    <div class="face" id="front">
+    <img src="Figuras/front.png">
+    </div>
+    <div class="face" id="back">
+      ${baralho[i]}
+    </div>
+</div>`
+  }
+}
+
+carregarBaralho ();
+
+
+ 
  /*Comandos de entrada de cartas */
 
-let escolhecartas = prompt("Com quantas cartas quer jogar?"); 
-/*while (cartas > 14  || cartas < 2) {
-    cartas = prompt("Com quantas cartas quer jogar?"); 
-
-} */ //Habilitar depois
+/*let escolhecartas = prompt("Com quantas cartas quer jogar?"); 
+while (escolhecartas > 14 || escolhecartas < 4 || escolhecartas%2 !== 0) {
+    escolhecartas = prompt("Com quantas cartas quer jogar?"); 
+}
+  //Habilitar depoiss
 // alert (escolhecartas);
-
-let cartasjogo = 14 - escolhecartas;
+/*
+let cartasjogo = 6 - escolhecartas;
 let aux = 1;
-// alert (cartasjogo);
  while (cartasjogo > 0) {
-    let removerCarta = document.querySelector(".carta" + aux);
+    let removerCarta = document.querySelector("main .carta" + aux);
     removerCarta.classList.add("remove")
     aux = aux + 1;
     cartasjogo = cartasjogo - 1;
-} 
+} */
 
-function virarCarta(parametro) {
-    let flip = document.querySelector("."+parametro )
-    flip.classList.add("verde");
+
+function fliparCarta(flip){
+const card = document.querySelector('.'+flip);
+card.classList.toggle('flip');
 }
