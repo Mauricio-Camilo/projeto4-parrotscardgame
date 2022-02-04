c66c8d017677c9e2f0467211d3b4375ceca5b739
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-02-04 19:59:35 -0300

    Correção do bug de clicar em 3 cartas ao mesmo tempo

--- scripts.js
+++ scripts.js
@@ -2,16 +2,17 @@
 let baralho = ['<img src="Gifs/bobrossparrot.gif">', '<img src="Gifs/explodyparrot.gif">', '<img src="Gifs/fiestaparrot.gif">', '<img src="Gifs/metalparrot.gif">', '<img src="Gifs/revertitparrot.gif">', '<img src="Gifs/tripletsparrot.gif">', '<img src="Gifs/unicornparrot.gif">'];
 let memoriaTexto = [];
 let memoriaCarta = [];
+let cartasFinalizadas = [];
 let cartasSalvas = 0;
 let quantidadeJogadas = 0;
-let escolhecartas = 0;
+let escolheCartas = 0;
 
 function iniciarJogo() {
-  escolhecartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
-  while (escolhecartas > 14 || escolhecartas < 4 || escolhecartas % 2 !== 0) {
-    escolhecartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
+  escolheCartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
+  while (escolheCartas > 14 || escolheCartas < 4 || escolheCartas % 2 !== 0) {
+    escolheCartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
   }
-  return (escolhecartas);
+  return (escolheCartas);
 }
 
 function comparador() {
@@ -21,7 +22,7 @@ function comparador() {
 function colocarCartas() {
   const carta = document.querySelector("main");
   carta.innerHTML = "";
-  for (let i = 0; i < escolhecartas; i++) {
+  for (let i = 0; i < escolheCartas; i++) {
     carta.innerHTML = carta.innerHTML + `
     <div class="flip carta${i} card" onclick="fliparCarta('carta${i}')" data-identifier="card">
     <div class="face frente" data-identifier="front-face">
@@ -31,13 +32,6 @@ function colocarCartas() {
       ${baralhoJogo[i]}
     </div>
 </div>`
-  }
-}
-
-function escolherCartas() {
-  let escolhecartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
-  while (escolhecartas > 14 || escolhecartas < 4 || escolhecartas % 2 !== 0) {
-    escolhecartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)");
   }
 }
 
@@ -54,11 +48,18 @@ function fliparCarta(flip) {
   else {
     if (memoriaTexto[0] !== memoriaTexto[1]) {
       habilitarFlip(memoriaCarta[0]);
+      let i = 0;
+      while (i < escolheCartas) {
+        desabilitarFlip("carta" + i);
+        i++;
+      }
       setTimeout(contarUmsegundo, 1000);
     }
     else {
       desabilitarFlip(memoriaCarta[0]);
       desabilitarFlip(memoriaCarta[1]);
+      cartasFinalizadas.push(memoriaCarta[0]);
+      cartasFinalizadas.push(memoriaCarta[1]);
       memoriaTexto = [];
       memoriaCarta = [];
       cartasSalvas++;
@@ -73,8 +74,8 @@ function desabilitarFlip(desabilita) {
 }
 
 function habilitarFlip(habilita) {
-  let travaCarta = document.querySelector("." + habilita);
-  travaCarta.classList.remove("disable");
+  let destravaCarta = document.querySelector("." + habilita);
+  destravaCarta.classList.remove("disable");
 }
 
 function contarUmsegundo() {
@@ -84,6 +85,16 @@ function contarUmsegundo() {
   }
   memoriaTexto = [];
   memoriaCarta = [];
+  i = 0;
+  while (i < escolheCartas) {
+    habilitarFlip("carta" + i);
+    i++;
+  }
+  let j = 0;
+  while (j < cartasFinalizadas.length) {
+    desabilitarFlip(cartasFinalizadas[j]);
+    j++;
+  }
 }
 
 function finalizarJogo() {
@@ -95,7 +106,7 @@ function finalizarJogo() {
 iniciarJogo();
 
 let baralhoJogo = [];
-distruibuirCartas = escolhecartas / 2;
+distruibuirCartas = escolheCartas / 2;
 for (let i = 0; i < distruibuirCartas; i++) {
   baralhoJogo.push(baralho[i]);
   baralhoJogo.push(baralho[i]);
